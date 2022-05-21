2970a9adb32ba477cf58e646ed02d82fe973931e
safayet-arman <safayetarman11@gmail.com>
2022-05-21 23:34:47 +0600

    forntend and admin page added by safayet

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved . Developed By - <a href="#"> safayet_arman</a></p>
+            </div>
+        </div>
+    
+
+    </body>
+</html>
